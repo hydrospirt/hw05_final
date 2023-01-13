764d6fd5db1bdb89f9d1239d5dd760acfe4925f6
Эдуард Гумен <hydrospirt@yandex.ru>
2023-01-13 20:09:31 +0300

    Replace in imports according to PEP8

--- yatube/posts/tests/test_cache.py
+++ yatube/posts/tests/test_cache.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.test import Client, TestCase
 from django.urls import reverse
+
 from posts.models import Post
 
 User = get_user_model()
